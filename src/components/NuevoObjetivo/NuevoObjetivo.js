c79f01eab32ae82eb25a3d4d999aab46a9f9a2ac
Your Name <garralonmourin@gmail.com>
2024-07-18 22:32:07 +0200

    Hacer que el texto sea dinámico con useState

--- src/components/NuevoObjetivo/NuevoObjetivo.js
+++ src/components/NuevoObjetivo/NuevoObjetivo.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./NuevoObjetivo.css";
 
 const NewGoal = (props) => {
+  const [textoObjetivo, setTextoObjetivo] = useState("");
+
   const addObjetivoHandler = (event) => {
     event.preventDefault();
 
     const nuevoObjetivo = {
       id: Math.random().toString(),
-      text: "Mi nuevo objetivo!",
+      text: textoObjetivo,
     };
-    console.log(nuevoObjetivo);
+
+    setTextoObjetivo("");
 
     props.onAddObjetivo(nuevoObjetivo);
   };
 
+  const textoObjetivoHandler = (event) => {
+    setTextoObjetivo(event.target.value);
+  };
+
   return (
     <form className="nuevo-objetivo" onSubmit={addObjetivoHandler}>
-      <input type="text" />
+      <input
+        type="text"
+        value={textoObjetivo}
+        onChange={textoObjetivoHandler}
+      />
       <button type="submit">Añadir Objetivo</button>
     </form>
   );
