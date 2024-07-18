d7575406795bd5b3a57889f1d53a508555bc1ffe
Your Name <garralonmourin@gmail.com>
2024-07-18 09:44:38 +0200

    Envío de Properties entre Componentes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./App.css";
 import ListaObjetivos from "./components/ListaObjetivos";
 
 const App = () => {
+  const objetivosCurso = [
+    { id: "oc1", text: "Finalizar el Curso" },
+    { id: "oc2", text: "Aprender sobre React" },
+    { id: "oc3", text: "Ayudar a otros estudiantes en las PyR" },
+  ];
+
   return (
     <div className="objetivos-curso">
       <h2>Objetivos del Curso React</h2>
-      <ListaObjetivos />
+      <ListaObjetivos objetivos={objetivosCurso} />
     </div>
   );
 };
--- src/components/ListaObjetivos.js
+++ src/components/ListaObjetivos.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./ListaObjetivos.css";
 
-const ListaObjetivos = () => {
+const ListaObjetivos = (props) => {
+  console.log(props.objetivos);
   return (
     <ul className="lista-objetivos">
-      <li>Finalizar el Curso</li>
-      <li>Aprender sobre React</li>
-      <li>Ayudar a otros estudiantes en las PyR</li>
+      {props.objetivos.map(objetivo => {
+        return <li key={objetivo.id}>{objetivo.text}</li>;
+      })}
     </ul>
   );
 };
