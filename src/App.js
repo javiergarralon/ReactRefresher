5feccd6de2f782e31744502d13a04056338a1a21
Your Name <garralonmourin@gmail.com>
2024-07-18 22:12:00 +0200

    El  uso de useState de la librería de React para añadir nuevos Cursos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
+import React, { useState } from "react";
 
 import ListaObjetivos from "./components/ListaObjetivos/ListaObjetivos";
 import NewGoal from "./components/NuevoObjetivo/NuevoObjetivo";
+import "./App.css";
 
 const App = () => {
-  const objetivosCurso = [
+  const [objetivosCurso, setObjetivoCurso] = useState([
     { id: "oc1", text: "Finalizar el Curso" },
     { id: "oc2", text: "Aprender sobre React" },
     { id: "oc3", text: "Ayudar a otros estudiantes en las PyR" },
-  ];
+  ]);
 
   const addNuevoObjetivoHandler = (nuevoObjetivo) => {
-    objetivosCurso.push(nuevoObjetivo);
-    console.log(objetivosCurso);
+    setObjetivoCurso(objetivosCurso.concat(nuevoObjetivo));
   };
 
   return (
